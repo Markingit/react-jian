ce1ff379aa2120ef7e510f187936ad008ecb8659
matengfei <matengfei@imzhiliao.com>
2019-02-14 15:33:50 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment, Component } from 'react';
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { GlobalStyle } from './style'
 import Header from './common/header'
+import Home from './pages/home'
+import Detail from './pages/detail'
 import store from './store'
 
 class App extends Component {
@@ -10,7 +13,15 @@ class App extends Component {
       <Fragment>
         <GlobalStyle />
         <Provider store={store}>
+          <div>
           <Header />
+            <BrowserRouter>
+              <div>
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/detail' exact component={Detail}></Route>
+              </div>
+            </BrowserRouter>
+          </div>
         </Provider>
       </Fragment>
      
